ef0141d6d90e6d8a5b0755abbb8a7fc060432428
rajdeep-office <rajdeep.das5@cognizant.com>
2025-08-13 18:17:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -69,5 +69,5 @@ app.get("*", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Backend running on http://localhost:${PORT}`);
+  console.log(`Backend running on ${PORT}`);
 });
